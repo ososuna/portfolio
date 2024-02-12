9ad73fafe18073d8127284d1424811014a5c3e26
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-02-12 17:13:45 -0600

    responsive design changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
 		extend: {},
+		screens: {
+			'xss': '360px',
+			'xs': '475px',
+			...defaultTheme.screens,
+		},
 	},
 	plugins: [],
 }
