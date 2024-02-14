04a00849245d79f39d95cf467eb14a1deb9b334e
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-02-14 11:02:42 -0600

    vercel web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [tailwind()]
 });
